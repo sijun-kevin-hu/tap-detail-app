a98f358c215fbc4a4bba505532339ce92beabd39
thekevinhu <22kevinhu@gmail.com>
2025-07-21 23:37:32 -0400

    feat: Integrate Sentry for error tracking and enhance .gitignore
    - Added Sentry configuration to next.config.ts for improved error monitoring in the application.
    - Updated package.json and package-lock.json to include the @sentry/nextjs dependency.
    - Enhanced .gitignore to exclude additional files related to Sentry and development tools.

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://ae3f410d14a9ee6589661ee8be17d828@o4509710086307840.ingest.us.sentry.io/4509710089584640",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
